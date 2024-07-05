62b6fdff92f0f20c3fbb42f5d986c619cd11a295
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2024-07-05 13:04:40 +0300

    Update score.validation.ts

--- src/validations/score.validation.ts
+++ src/validations/score.validation.ts
@@ -1,6 +1,14 @@
 import Joi from 'joi';
 
 export const scoreSchema = Joi.object({
-  name: Joi.string().required(),
-  score: Joi.number().required(),
+  name: Joi.string().required().messages({
+    'string.base': 'Имя должно быть строкой.',
+    'string.empty': 'Имя не может быть пустым.',
+    'any.required': 'Имя является обязательным полем.',
+  }),
+  score: Joi.number().required().messages({
+    'number.base': 'Очки должны быть числом.',
+    'number.empty': 'Очки не могут быть пустыми.',
+    'any.required': 'Очки являются обязательным полем.',
+  }),
 });
